cf60b299a57d3424861a53cd3053291d47b7b786
Lautaro Cepeda <lautac29@gmail.com>
2020-02-19 22:18:53 -0300

    added fields User Model

--- models/user.model.js
+++ models/user.model.js
@@ -7,29 +7,68 @@ const { TE, to }    = require('../services/util.service');
 const CONFIG        = require('../config/config');
 
 
-
+//TODO agregar authprovider y ouid providar - Social logins
 module.exports = (sequelize, DataTypes) => {
 
     const Model = sequelize.define('User', {
-        name: DataTypes.STRING,
-        email: { type: DataTypes.STRING, allowNull: true, unique: true, validate: { isEmail: { msg: "Email invalid." } } },
-        phone: { type: DataTypes.STRING, allowNull: true, validate: { len: { args: [7, 20], msg: "Phone number invalid, too short." }, isNumeric: { msg: "Not a valid phone number." } } },
-        password: DataTypes.STRING
-    });
+        oauth_provider: { 
+            type: DataTypes.ENUM('app', 'facebook'),
+            allowNull: false,
+            defaultValue: 'app'
+        },
+        oauth_uid: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: "Email invalid."
+                }
+            }
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [7, 20],
+                    msg: "Phone number invalid, too short."
+                },
+                isNumeric: {
+                    msg: "Not a valid phone number."
+                }
+            }
+        },
+        birthday: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        encript_pw: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    })
 
 
     Model.beforeSave(async (user, options) => {
         let err;
 
-        if (user.changed('password')) {
+        if (user.changed('encript_pw')) {
             let salt, hash;
             [err, salt] = await to(bcrypt.genSalt(10));
             if (err) TE(err.message, true);
 
-            [err, hash] = await to(bcrypt.hash(user.password, salt));
+            [err, hash] = await to(bcrypt.hash(user.encript_pw, salt));
             if (err) TE(err.message, true);
 
-            user.password = hash;
+            user.encript_pw = hash;
         }
     });
 
@@ -37,9 +76,9 @@ module.exports = (sequelize, DataTypes) => {
     // compare form password with database hash password
     Model.prototype.comparePassword = async function (pw) {
         let err, pass
-        if (!this.password) TE('Ingresa tu contraseña');
+        if (!this.encript_pw) TE('Ingresa tu contraseña');
 
-        [err, pass] = await to(bcrypt_p.compare(pw, this.password));
+        [err, pass] = await to(bcrypt_p.compare(pw, this.encript_pw));
         if (err) TE(err);
 
         if (!pass) TE('Ups! Contraseña incorrecta. Vuelve a intentarlo');
@@ -57,9 +96,11 @@ module.exports = (sequelize, DataTypes) => {
     Model.prototype.toWeb = function () {
         return {
             id: this.id,
+            provider: this.oauth_provider,
             name: this.name,
             email: this.email,
-            phone: this.phone
+            birthday: this.birthday,
+            phone: this.phone,
         };
     };
 
